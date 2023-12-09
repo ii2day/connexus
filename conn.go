6d8f19a77a55f55f749509149f89b86ac3b37288
ii2day <ji.li@daocloud.io>
2023-12-09 16:29:10 +0800

    init project
    Signed-off-by: ii2day <ji.li@daocloud.io>

---
+++ conn.go
@@ -0,0 +1,40 @@
+package connexus
+
+import (
+	"net"
+	"time"
+)
+
+type Connex struct {
+	net.Conn
+	cp          *connexPool
+	updatedTime time.Time
+	unusable    bool
+}
+
+// Close put the connection back to pool if possible.
+// Executed by multi times is ok.
+func (c *Connex) Close() error {
+	if c.unusable {
+		return c.close()
+	}
+
+	c.updatedTime = time.Now()
+	if err := c.cp.put(c); err != nil {
+		c.cp = nil
+		return c.close()
+	}
+	return nil
+}
+
+func (c *Connex) MarkUnusable() {
+	c.unusable = true
+}
+
+func (c *Connex) IsUnusable() bool {
+	return c.unusable
+}
+
+func (c *Connex) close() error {
+	return c.Conn.Close()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ii2day/connexus
+
+go 1.20
